6b96d34cff24dc587c6ffa51692553d2229e007a
Joseph Crandall <jwcrand@gmail.com>
2023-09-06 11:45:05 -0400

    update

--- lab2exercise1.s
+++ lab2exercise1.s
@@ -1,14 +1,14 @@
-.data			; start of data segment
-A:	.word32 10	; create a 32-bit work with value 10 (decimal), A represents the data address (0x0000)
-B: 	.word32 8
-C: 	.word32 0
+.data				; start of data segment
+s:	.word32 0		; create a 32-bit word with value 0 (decimal)
+i: 	.word32 1		; create a 32-bit word with value 1 (decimal)
+N: 	.word32 101		; create a 32-bit word with value 101 (decimal)
 
-.text			; start of code segment
-main:			; label indicating the start of the program, lable represetns the instruction address (0x0000)
+.text				; start of code segment
+main:				; label indicating the start of the program, lable represetns the instruction address (0x0000)
 
-	ld r4, A(r0)
-	ld r5, B(r0)
+	ld r4, s(r0)
+	ld r5, i(r0)
 	dadd r3, r4, r5
 	dadd r3, r3, r3
-	sd r3, C(r0)
+	sd r3, N(r0)
 	halt
